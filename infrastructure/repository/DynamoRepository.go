d01db68f3ba7a6bef156f15bdebea104056ce569
Camilo Diaz <camilo.diaz@avaldigitallabs.com>
2021-04-08 20:21:20 -0500

    Changed import reference in Repository

--- infrastructure/repository/DynamoRepository.go
+++ infrastructure/repository/DynamoRepository.go
@@ -4,7 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	repository2 "github.com/camilodiazj/mutants/application/repository"
+	"github.com/camilodiazj/mutants/application/repository"
 	"log"
 	"strconv"
 )
@@ -14,13 +14,13 @@ type dynamoRepository struct {
 	dynamo *dynamodb.DynamoDB
 }
 
-func NewDynamoRepository(tableName string) repository2.DnaRepository {
+func NewDynamoRepository(tableName string) repository.DnaRepository {
 	return &dynamoRepository{
 		table:  tableName,
 		dynamo: connectDynamoDb()}
 }
 
-func (r *dynamoRepository) Save(dna *repository2.DnaEntity) error {
+func (r *dynamoRepository) Save(dna *repository.DnaEntity) error {
 	dynamo := r.dynamo
 	_, err := dynamo.PutItem(&dynamodb.PutItemInput{
 		Item: map[string]*dynamodb.AttributeValue{
@@ -42,7 +42,7 @@ func (r *dynamoRepository) Save(dna *repository2.DnaEntity) error {
 	return nil
 }
 
-func (r *dynamoRepository) CountMutants() (*repository2.Counter, error) {
+func (r *dynamoRepository) CountMutants() (*repository.Counter, error) {
 	dynamo := r.dynamo
 	params := &dynamodb.ScanInput{
 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
@@ -59,10 +59,10 @@ func (r *dynamoRepository) CountMutants() (*repository2.Counter, error) {
 
 	if err != nil {
 		log.Fatalf("Query API call failed: %s", err)
-		return &repository2.Counter{}, err
+		return &repository.Counter{}, err
 	}
 
-	return &repository2.Counter{
+	return &repository.Counter{
 		CountResult: uint64(*result.Count),
 		TotalCount:  uint64(*result.ScannedCount),
 	}, nil
